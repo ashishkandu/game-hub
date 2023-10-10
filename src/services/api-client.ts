f13dfce102e0d71d3c772d224f1da4e8621c6da2
ashishkandu <ashishkandu43@gmail.com>
2023-10-10 20:18:41 +0545

    key changed

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'd2382ee010494c51a83f6a9b5b54efa1',
+    key: import.meta.env.VITE_API_KEY,
   },
 });
